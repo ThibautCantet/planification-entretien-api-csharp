425c2e1e1a20593e5f46a7fc8e716b5c02d4da48
Thibaut Cantet <thibaut.cantet@soat.fr>
2022-04-14 19:07:22 +0200

    refacto to clean archi second step

--- src/PlanificationEntretien/Infrastructure/Controllers/CandidatController.cs
+++ src/PlanificationEntretien/Infrastructure/Controllers/CandidatController.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
-using PlanificationEntretien.Infrastructure.Models;
+using PlanificationEntretien.Domain;
 using PlanificationEntretien.UserCase;
 
 namespace PlanificationEntretien.Infrastructure.Controllers
